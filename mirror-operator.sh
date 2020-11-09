a99588d40ad29b976befc58e58f81b949f42f09f
范培伦 <fanpeilun@163.com>
2020-11-09 22:38:15 +0800

    ocp4.6

--- mirror-operator.sh
+++ mirror-operator.sh
@@ -13,6 +13,8 @@ echo "********** Check tools **********"
 type oc
 type jq
 type skopeo 
+type grpcurl
+type podman
 
 echo -e "\n********** Check openshift session **********"
 echo "USER: $(oc whoami)"
@@ -63,9 +65,15 @@ echo -e "\n********** 1. Make directory  $DIR **********"
 mkdir $DIR 
 cd $DIR
 eval "oc get packagemanifests $PACKAGE -o go-template='{{range .status.channels}}{{if eq .name \"$CHANNEL\" }}{{range .currentCSVDesc.relatedImages}}{{.}}{{\"\\n\"}}{{end}}{{end}}{{end}}'" > images
+echo -e "\n********** 2. Get bundle image **********"
+#Get bundle image
+podman pull $(oc get catalogsource $CATALOGSOURCE -o json | jq -r .spec.image)
+podman run -p50051:50051 --name=tmp -d --rm $(oc get catalogsource $CATALOGSOURCE -o json | jq -r .spec.image) > /dev/null
+sleep 3
+eval "grpcurl -plaintext -d '{\"pkgName\":\"$PACKAGE\",\"channelName\":\"$CHANNEL\"}' localhost:50051 api.Registry/GetBundleForChannel | jq -r .bundlePath >> images"
+podman stop tmp > /dev/null
 cat images |sed 's|\(.*\)|&=\1|g' |sed "s|=.*/\([^/]*\)/\(.*\)@.*|=$REG/\1/\2|g" > mirror-list
-rm images
-echo -e "\n********** 2. Make imageContentSourcePolicy.yaml **********"
+echo -e "\n********** 3. Make imageContentSourcePolicy.yaml **********"
 echo -e -n "apiVersion: operator.openshift.io/v1alpha1
 kind: ImageContentSourcePolicy
 metadata:
@@ -81,50 +89,56 @@ while read line; do
     echo "      source: $source"
 done < mirror-list >> imageContentSourcePolicy.yaml
 
-echo -e "\n********** 3. Pull image to local dir \"image-cache\"**********"
-mkdir image-cache
+echo -e "\n********** 4. Pull image **********"
+select opt in {"cache image to local dir","mirror image to $REG immediately"};do
+case "$REPLY" in
+    1) PUSH=1 ; break;;
+    2) PUSH=2 ; break;;
+    *) echo "Bad option";
+esac
+done
+
+mkdir cache
 index=0
 count=$(cat mirror-list | wc -l)
 while read line; do
     let index+=1
     src=$(echo $line | cut -d= -f1)
-    dest=$(echo $line | cut -d/ -f3 |cut -d= -f1)
-    echo "($index/$count)Pulling $src"
-    skopeo copy --authfile=$AUTH --all docker://$src dir:image-cache/$dest
+    if [ "$PUSH" == "1" ]; then
+      echo "($index/$count)Pulling $src"
+      break
+      skopeo copy --authfile=$AUTH --all docker://$src dir:cache/$(echo $line | cut -d/ -f3 |cut -d= -f1)
+    else
+      echo "($index/$count)Mirroring $src"
+      skopeo copy --authfile=$AUTH --all docker://$src docker://$(grep $src mirror-list | cut -d= -f2)
+    fi
 done < mirror-list
 
 echo '
 index=0
 count=$(cat mirror-list | wc -l)
 read -p "Path of the authentication file: " AUTH
-for i in $(ls image-cache); do
+for i in $(ls cache); do
  let index+=1
  dest=$(grep $i mirror-list | cut -d= -f2)
  echo "($index/$count)Pushing $dest"
- skopeo copy --authfile=$AUTH --all dir:image-cache/$i docker://$dest
+ skopeo copy --authfile=$AUTH --all dir:cache/$i docker://$dest
 done' > push.sh
 chmod +x push.sh
 
-echo -e "\n********** It's time to push images to private registry. **********"
-echo -e "There will be two steps:\n1. push local images to private.\n2. create imageContentSourcePolicy(icsp) to openshift cluster.\n"
-read -p "Do you want to do it right now?(y/n): " answer
+read -p "Do you want to create ImageContentSourcePolicy(icsp) to openshift cluster right now?(y/n): " answer
 if [ "$answer" = "y" ]; then 
-    step1="./push.sh"
-    step2="oc create -f imageContentSourcePolicy.yaml"
-    echo "1. $step1"
-    eval $step1
-    read -p "2. $step2, all hosts will restart! Continue(y/n): " icsp
-    if [ "$icsp" = "y" ]; then
-       eval $step2
-       echo "All done, Enjoy!"
-    else
-       echo "All right,Please do it manually. Enjoy!"
-    fi  
+    oc apply -f imageContentSourcePolicy.yaml 
+    echo "All done, Enjoy!"
 else
     echo -e "\nAll right,You can do it when you need it.\nProcedure:"
     echo -e "1. Copy dir $DIR to your bastion machine. "
     echo -e "2. Change  the current directory to $DIR,\033[31m\"cd $DIR\"\033[0m"
-    echo -e "2. Run script file,\033[31m\"sh push.sh\"\033[0m"
-    echo -e "3. Create icsp,\033[31m\"oc create -f imageContentSourcePolicy.yaml\"\033[0m"
-    echo -e "4. Enjoy!"
+    if [ "$PUSH" == "1" ]; then
+    echo -e "3. Run script file,\033[31m\"sh push.sh\"\033[0m"
+    echo -e "4. Create icsp,\033[31m\"oc apply -f imageContentSourcePolicy.yaml\"\033[0m"
+    else
+    echo -e "3. Create icsp,\033[31m\"oc apply -f imageContentSourcePolicy.yaml\"\033[0m"
+    fi
+    echo -e "Enjoy!"
 fi
